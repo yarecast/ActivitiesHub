58df166f10f89ff5243b25f459ef23515cbf36f9
yarecast <yarecasst@gmail.com>
2026-09-10 11:29:10 -0600

    parcial 01- add skeleton app

--- web/EventsHub/vite.config.ts
+++ web/EventsHub/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   server: {
+      port: 3000
+    },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
